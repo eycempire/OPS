82e60a573ec3e7c261bb03314349172bffc3913e
eycempire <eycfde@gmail.com>
2026-05-14 14:01:31 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 // لو اتغير الـ Apps Script URL — غيّره هنا بس
 // وكل الصفحات هتاخده تلقائياً
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz6ZRse1k0-P30fQwXBS_eyg2CoMerRx35cmgBcxaTZQUccg_k6k1woXqGTSs0I9ts9XQ/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxqZHSK7qNLugzrQ-VNf79FE4ogyPwdNCLx9_zGXHTqDw5V4FPMt9vQ7l5PBc9knJz4yw/exec";
